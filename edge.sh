2c611c41cca527045999d195380df4ddfb7315cf
Matt De Young <giftie61@hotmail.com>
2014-12-06 17:54:31 -0700

    Update edge.sh

--- edge.sh
+++ edge.sh
@@ -4,8 +4,13 @@
 if [ -z "$EDGE" ]; then
   echo "Bleeding edge not requested"
 else
+  echo "Bleeding edge has been requested"
   apt-get install -qy git
   rm -rf /opt/sickrage
   git clone https://github.com/SiCKRAGETV/SickRage.git /opt/sickrage
+  cd /opt/sickrage
+  git checkout master
+  git checkout develop
+  cd /
   chown -R nobody:users /opt/sickrage
 fi
