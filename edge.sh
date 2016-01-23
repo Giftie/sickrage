a8b39371dbbf6f70bfaa8be94530359f778f8bb3
Matt De Young <giftie61@hotmail.com>
2016-01-22 22:22:14 -0700

    fix last git link

--- edge.sh
+++ edge.sh
@@ -7,7 +7,7 @@ else
   echo "Bleeding edge has been requested"
   apt-get install -qy git
   rm -rf /opt/sickrage
-  git clone https://github.com/SiCKRAGETV/SickRage.git /opt/sickrage
+  git clone https://github.com/SickRage/SickRage.git /opt/sickrage
   cd /opt/sickrage
   git checkout master
   git checkout develop
