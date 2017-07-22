2ceb674f260d9c89c9517d040b7ab8617e85689b
Matt De Young <giftie61@hotmail.com>
2017-07-21 23:03:38 -0600

    Update edge.sh
    return to previous github

--- edge.sh
+++ edge.sh
@@ -7,7 +7,7 @@ else
   echo "Bleeding edge has been requested"
   apt-get install -qy git
   rm -rf /opt/sickrage
-  git clone https://git.sickrage.ca/SiCKRAGE/sickrage.git /opt/sickrage
+  git clone https://github.com/SiCKRAGETV/SickRage.git /opt/sickrage
   cd /opt/sickrage
   git checkout master
   git checkout develop
